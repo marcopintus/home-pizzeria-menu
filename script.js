393a846df2ee843eeab383952e6ede77d2bc8b98
marcopintus <marco.pintus93@gmail.com>
2022-06-11 14:12:28 +0200

    Defined general site layout. Added all the pizzas to the menu

---
+++ script.js
@@ -0,0 +1,152 @@
+
+let classicPizzas = {
+    focaccia:{
+        name: "Focaccia",
+        ingredients: "Olio, rosmarino",
+        price: "0 €",
+    },
+    marinara:{
+        name: "Marinara",
+        ingredients: "Pomodoro, origano",
+        price: "0 €",
+    },
+    margherita:{
+        name: "Margherita",
+        ingredients: "Pomodoro, mozzarella",
+        price: "0 €",
+    },
+    calzone:{
+        name: "Calzone",
+        ingredients: "Pomodoro, mozzarella, prosciutto cotto",
+        price: "0 €"
+    }
+};
+
+let ourPizzas = {
+    formaggiPiero:{
+        name: "I formaggi di Piero",
+        ingredients: "Mozzarella, grana, casizolu, gorgonzola",
+        price: "0 €"
+    },
+    annaCapelliRossi: {
+        name: "Anna dai capelli rossi",
+        ingredients: "Pomodoro, uovo (occhio di bue)",
+        price: "0 €"
+    },
+    ortolanaSaporita:{
+        name:"Ortolana Saporita",
+        ingredients: "Pomodoro, mozzarella, zucchine grigliate, casizolu (rubato da I 'formaggi di Piero')",
+        price: "0 €"
+    },
+    rosmarinoConPatate:{
+        name: "Rosmarino con patate",
+        ingredients: "Pomodoro, patate al forno, rosmarino, rosmarino",
+        price: "0 €"
+    },
+    fondoMare:{
+        name: "In fondo al mare",
+        ingredients: "Pomodoro, mozzarella, frutti di mare",
+        price: "0 €"
+    },
+    oroGiallo:{
+        name: "Oro giallo",
+        ingredients: "Pomodoro, mozzarella, bottarga",
+        price: "0 €"
+    },
+    quelloCE:{
+        name:"Quello che c'è",
+        ingredients: "Ingredienti a scelta tra quelli disponibili",
+        price: "0 €"
+    },
+    tricolore:{
+        name: "Tricolore",
+        ingredients: "Pomodoro, mozzarella, rucola, grana (fuori cottura)",
+        price: "0 €"
+    },
+    carbonara:{
+        name: "Carbonara",
+        ingredients: "Mozzarella, uovo, pancetta",
+        price: "0 €"
+    },
+    carbonaraRossa:{
+        name: "Carbonara",
+        ingredients: "Pomodoro, mozzarella, uovo, pancetta",
+        price: "0 €"
+    },
+    fuocoFiamme:{
+        name: "Fuoco e fiamme",
+        ingredients: "Mozzarella, casizolu, gorgonzola. salsiccia piccante",
+        price: "0 €"
+    },
+    daTutteUnPo:{
+        name:"Da tutte un po'",
+        ingredients: "Prendere qualche ingrediente dalle altre pizze in preparazione",
+        price: "0 €"
+    },
+    casizoluGuanciale:{
+        name: "Casizolu e Guanciale",
+        ingredients: "Mozzarella, casizolu, guanciale",
+        price: "0 €"
+    },
+    comePantera:{
+        name: "Come a La Pantera",
+        ingredients: "Pomodoro, mozzarella, patate al forno, rosmarino, prosciutto cotto",
+        price: "0 €"
+    },
+    melagriglio:{
+        name: "Melagriglio",
+        ingredients: "Pomodoro, mozzarella, melanzane grigliate",
+        price: "0 €"
+    },
+    parmigiana:{
+        name: "Parmigiana",
+        ingredients: "Pomodoro, mozzarella, melanzene fritte, parmigiano",
+        price: "0 €"
+    }
+};
+
+let softDrinks = ['Acqua Naturale', 'Acqua Frizzante', 'Coca-cola','Tè alla pesca','Heineken'];
+
+
+function createPizzaCategory(pizzaCategory,pizzas){
+
+    let pizzaMenu = document.querySelector(".pizza-menu");
+    let category = document.createElement("div");
+    category.classList.add("pizza-type");
+    category.textContent = pizzaCategory;
+    pizzaMenu.appendChild(category);
+
+    let pizzasContainer = document.createElement("div");
+    pizzasContainer.classList.add("pizzas");
+    pizzaMenu.appendChild(pizzasContainer);
+
+    for (let [key,value] of Object.entries(pizzas)){
+        
+        let pizza = document.createElement("div");
+        pizza.classList.add("pizza");
+        pizzasContainer.appendChild(pizza);
+
+        let pizzaName = document.createElement("p");
+        pizzaName.classList.add("pizza-name");
+        pizzaName.textContent = value.name;
+        pizza.appendChild(pizzaName)
+
+        let pizzaIngredients = document.createElement("p");
+        pizzaIngredients.classList.add("pizza-ingredients");
+        pizzaIngredients.textContent = value.ingredients;
+        pizza.appendChild(pizzaIngredients)
+
+        /*
+        let pizzaPrice = document.createElement("p");
+        pizzaPrice.classList.add("pizza-price");
+        pizzaPrice.textContent = value.price;
+        pizza.appendChild(pizzaPrice)
+        */
+
+    }
+}
+
+
+createPizzaCategory("Pizze Classiche",classicPizzas)
+createPizzaCategory("Pizze della casa",ourPizzas)
+
